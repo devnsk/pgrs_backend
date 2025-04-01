392959e8deaa46e007a50dab949b2ceccdcde5df
devnsk <devnsk.2024@gmail.com>
2025-04-01 07:23:11 +0530

    initial commit

---
+++ src/main/java/org/pgrs/PgrsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pgrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgrsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgrsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pgrs/PgrsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pgrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgrsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
